a127b3970d1a13c93f10711b56333cf3d85b2609
EsheBT <eshetu9244@yahoo.com>
2023-04-21 12:45:14 -0700

    shell sort

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,46 +1,42 @@
 #include "sort.h"
-#include <stdio.h>
-/**
- * _swap - swaped 2 values.
- * @array: the array for swap him values.
- * @i: First index
- * @j: Second index
- * Return: Nothing
- */
-void _swap(int *array, int i, int j)
-{
-	int tmp;
-
-	if (array[i] != array[j])
-	{
-		tmp = array[i];
-		array[i] = array[j];
-		array[j] = tmp;
-	}
-}
 
 /**
- * shell_sort - sort the list and print the changes
- * @array: The array to sort.
- * @size: Size of the array
- * Return: Nothing
+ * shell_sort - sorts an array of integers in ascending order using a shell
+ * sort algorithm, with gap sizes determined by a decreasing Knuth seqeuence
+ * @array: array of integers to be sorted
+ * @size: amount of elements in array
  */
 void shell_sort(int *array, size_t size)
 {
-	size_t h = 0,  i, j;
+	int i, j, gap, n, Knuth_max, temp;
 
-	if (size < 2)
+	if (!array || size < 2)
 		return;
 
-	while (h <= size / 3)
-		h = h * 3 + 1;
-
-	while (h >= 1)
+	n = (int)size;
+	for (gap = 1; gap < n; gap = (gap * 3) + 1)
 	{
-		for (i = h; i < size; i++)
-			for (j = i; j >= h && array[j] < array[j - h]; j -= h)
-				_swap(array, j, j - h);
-		h /= 3;
+		Knuth_max = gap;
+	}
+/* Start with the largest Knuth seq value less than n as gap, */
+/* and work down sequence to a gap of 1 */
+	for (gap = Knuth_max; gap > 0; gap = (gap - 1) / 3)
+	{
+		/* Do a gapped insertion sort for this gap size. */
+		for (i = gap; i < n; i++)
+		{
+			/* add array[i] to gap sorted elements; */
+			/* save array[i] in temp in preparation to overwrite */
+			temp = array[i];
+			/* shift earlier gap-sorted elements up until the */
+			/* correct location for array[i] is found */
+			for (j = i; j >= gap && array[j - gap] > temp; j -= gap)
+			{
+				array[j] = array[j - gap];
+			}
+/* temp original array[i]) to its correct location */
+			array[j] = temp;
+		}
 		print_array(array, size);
 	}
 }
